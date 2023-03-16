334ddf0e8b556cf8c9d8a40b8b2062394b71574c
Abhay Thanak <abhaythanak@gmail.com>
2023-03-16 13:48:19 +0530

    react challenges 2

---
+++ react-challenges/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// import ClassComponents from './components/ClassComponents';
+import Props from './components/Props';
+// import Map from "./components/Map"
+// import States from './components/States';
+
+function App() {
+  return (
+    
+    <div className='app'> 
+    {/* <Map/> */}
+    {/* <States/> */}
+    {/* <ClassComponents/> */}
+    <Props/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-challenges/src/components/ClassComponents.jsx
@@ -0,0 +1,40 @@
+// import { Component } from "react";
+
+
+
+// export default class ClassComponents extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             name: "Abhay",
+//             age: 0,
+//             array: [1, 2, 3, 4, 5],
+//         };
+//     }
+
+//     changeStatus = () => {
+//         this.setState({ 
+//             age: this.state.age+= 2 ,
+//             name: "Uday"
+//          });
+//     }
+    
+
+
+//     render() {
+//         return(
+//             <div className="">
+//                 <h1>{this.state.name}</h1>
+//                 <h1>{this.state.age}</h1>
+//                 <h1>{this.state.array}</h1>
+
+//                 {/* {this.state.array.map((item) => {
+//                     return item * 2
+//                 })} */}
+
+//                 <button onClick={this.changeStatus}>change state</button>
+
+//             </div>
+//         )
+//     }
+// };
