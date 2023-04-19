be31f39683b0b26b81536a4c13525b64288e2e1b
Abhay Thanak <abhaythanak@gmail.com>
2023-04-19 21:12:26 +0530

    Register and Login page Complited

---
+++ trauma-chat/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Register from './components/pages/Register'
+import Login from './components/pages/Login'
+
+
+function App() {
+  
+
+  return (
+    <div className="">
+      {/* <Register/> */}
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ trauma-chat/src/components/pages/Home.jsx
@@ -0,0 +1,6 @@
+
+export default function Home(params) {
+    return(
+        <h1>Home</h1>
+    )
+};
