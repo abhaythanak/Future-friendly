54e558c9b62c26dedcd8c1ced4a16be2a059e1ee
abhaythanak <abhaythanak@gmail.com>
2024-09-19 16:28:33 +0530

    optimize the canvas for optimization in 3d character and UI related update

--- 3d-portfolio/src/components/About.jsx
+++ 3d-portfolio/src/components/About.jsx
@@ -1,73 +1,54 @@
-import React from 'react'
-//import Tilt from 'react-tilt'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
-import { styles } from '../styles'
-import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
-import { SectionWrapper } from '../hoc'
-
-
-const ServiceCard = ({ index, title, icon}) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div className="xs:w-[250px] w-full">
+    <div className="w-full sm:w-[200px] md:w-[220px] lg:w-[250px] xs:w-[250px]">
       <motion.div
-      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className="shadow-card p-[1px] rounded-[20px] w-full green-pink-gradient"
       >
-        <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
-      </div>
+        <div className="flex flex-col justify-evenly items-center bg-tertiary px-6 sm:px-8 lg:px-12 py-5 rounded-[20px] min-h-[200px] sm:min-h-[240px] lg:min-h-[280px]">
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-12 sm:w-14 lg:w-16 h-12 sm:h-14 lg:h-16 object-contain"
+          />
+          <h3 className="font-bold text-[18px] text-center text-white sm:text-[20px] lg:text-[22px]">
+            {title}
+          </h3>
+        </div>
       </motion.div>
-
     </div>
-    
-  )
-}
-
+  );
+};
 
 const About = () => {
   return (
     <>
-    <motion.dev variants={textVariant()}>
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview</h2>
-    </motion.dev>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
 
-    <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 max-w-xl lg:max-w-3xl text-[15px] text-secondary sm:text-[16px] lg:text-[17px] leading-[25px] lg:leading-[30px]"
       >
-        I am a skilled frontend developer with extensive experience in JavaScript,
-        specializing in frameworks such as React and Next.js. I take pride in my ability to 
-        swiftly grasp new concepts and thrive on collaborating closely with clients to develop efficient,
-        scalable, and user-friendly solutions that tackle real-world challenges.
-        If you have exciting ideas waiting to be realized, 
-        I'm here and eager to work together with you to bring those ideas to life!
+        I am a skilled frontend developer with extensive experience in JavaScript, specializing in frameworks such as React and Next.js. I take pride in my ability to swiftly grasp new concepts and thrive on collaborating closely with clients to develop efficient, scalable, and user-friendly solutions that tackle real-world challenges. If you have exciting ideas waiting to be realized, I'm here and eager to work together with you to bring those ideas to life!
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="flex flex-wrap justify-center gap-6 lg:gap-10 mt-10 lg:mt-20">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About,"about")
+export default SectionWrapper(About, 'about');
--- 3d-portfolio/src/components/Hero.jsx
+++ 3d-portfolio/src/components/Hero.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+
+// import { ComputersCanvas } from "./canvas";
+const ComputersCanvas = React.lazy(() => import("./canvas/Computers"));
 
 const Hero = () => {
   return (
@@ -10,8 +13,8 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='bg-[#915EFF] rounded-full w-5 h-5' />
+          <div className='w-1 h-40 sm:h-80 violet-gradient' />
         </div>
 
         <div>
@@ -27,9 +30,9 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='bottom-32 xs:bottom-10 absolute flex justify-center items-center w-full'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='flex justify-center items-start border-4 border-secondary p-2 rounded-3xl w-[35px] h-[64px]'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -39,7 +42,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className='bg-secondary mb-1 rounded-full w-3 h-3'
             />
           </div>
         </a>
